46f8374ca5274ac4fcf9e741d14e8335492902fa
Eugene Brigita <54357438+eugenebrigita@users.noreply.github.com>
2020-10-04 17:48:31 +0700

    Create myscript.sh

---
+++ TXT/myscript.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Eugene Brigita Lauw
+# Sun Oct 04 17:20 WIB
+
+FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+SHA="SHA256SUM"
+
+# delete file if there existe #SHA or #SHA.asc
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"  # print to be executed script
+sha256sum $FILES > $SHA         
+# sha256sum created checksum for $FILES (mylog.txt, mypubkey.txt, myrank.txt, myscript.sh)
+# the result of sha256sum will be written to $SHA (SHA256SUM)
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA               # command to verified files integrity of $SHA (SHA256SUM)
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA    
+# gpg is scripting and signing tools
+# -o to show output of $HA.asc (SHA256SUM.asc)
+# -a to let gpg know, ecrypting with ASCII format
+# -sb to sign the contents of file SHA256SUM to SHA256SUM.asc
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA      # to verify the file's integrity, either the output is correct or not
+
+exit 0
+
+# Sun Oct 04 17:48 WIB
